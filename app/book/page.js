395184d3b6217c409bf87879ec65f01f754570a7
millionaires26 <millionairesnook@gmail.com>
2026-08-07 19:18:00 -0700

    Create Krovoro booking page

---
+++ app/book/page.js
@@ -0,0 +1,11 @@
+export default function BookingPage() {
+  return (
+    <main>
+      <h1>Book a Demo</h1>
+      <p>
+        See how Krovoro can answer calls, qualify leads,
+        and book appointments automatically.
+      </p>
+    </main>
+  );
+}
